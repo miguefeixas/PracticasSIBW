1873dbd0dcc1c69e7b9787eeb3e6d652852bf86b
José Miguel Feixas Galdeano <miguefeixas@gmail.com>
2023-03-17 14:40:56 +0100

    [P2] Button working and design done

--- web/js/comments.js
+++ web/js/comments.js
@@ -1,9 +1,30 @@
-/*let openCommentsButton = document.getElementById("openCommentsButton");
-let comments = document.getElementById("commentsSidebar");*/
+/* José Miguel Feixas Galdeano - Curso 22/23 */
 
-function openNav() {
-    document.getElementById("commentsSidebar").style.display = "block";
+/**
+ * Declaración de variables para guardar elementos del DOM
+ */
+
+let openCommentsButton = document.getElementById("openCommentsButton");
+let closeCommentsButton = document.getElementById("closeCommentsButton");
+let sendCommentButton = document.getElementById("sendCommentButton");
+let comments = document.getElementById("commentsSidebar");
+
+/**
+ * Declaración de funciones
+ */
+
+function openComments() {
+    comments.style.display = "block";
 };
 
-document.getElementById("openCommentsButton").onclick = function(){openNav()};
+function closeComments() {
+    comments.style.display = "none";
+};
+
+/**
+ * Funcionalidades
+ */
+
+openCommentsButton.onclick = function(){ openComments() };
+closeCommentsButton.onclick = function(){ closeComments() };
 
